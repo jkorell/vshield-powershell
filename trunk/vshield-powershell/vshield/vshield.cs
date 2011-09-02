6722b88a939197852f00789482943d4d21d12048
jcpowermac@gmail.com <jcpowermac@gmail.com@746e9b51-8f0a-b68f-294a-66adf4e6bd98>
2011-09-02 13:46:18 +0000

    updates

---
+++ trunk/vshield-powershell/vshield/vshield.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Management.Automation;
+using System.ComponentModel;
+
+using RestSharp;
+
+
+
+namespace vshield
+{
+    [RunInstaller(true)]
+    public class vshieldsnapin : PSSnapIn
+    {
+        public override string Description
+        {
+            get { return "Snapin to provide vshield operations"; }
+        }
+
+        public override string Name
+        {
+            get { return "vshieldsnapin"; }
+        }
+
+        public override string Vendor
+        {
+            get { return "CSC"; }
+        }
+    }
+
+
+    
+
+}
